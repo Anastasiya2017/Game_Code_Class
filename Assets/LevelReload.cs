ef7a69d66ad2ed8fafd531660cc7f22c3ec35b65
Asya <alleksa_anastasiya@mail.ru>
2021-02-20 20:34:35 +0300

    3.СИСТЕМЫ ЧАСТИЦ, ТРИГГЕРЫ, ПЕРЕЗАГРУЗКА УРОВНЯ

---
+++ Assets/LevelReload.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelReload : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        //загружаем сцену, получая название текущей сцены
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
